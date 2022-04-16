09c7fcddc125d698e7ff707b6895a4602c0f8304
李志刚-Home <lizhigang@lizhigangdeMacBook-Pro.local>
2022-04-16 10:21:30 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+  <h1>{{ msg }}</h1>
+  <Comp />
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+ <button @click="onclick">emit</button>
+</template>
+
+<script setup>
+import { ref ,defineProps,defineEmits} from "vue";
+//1.直接导入组件
+import Comp from "comps/Comp.vue";
+//2.属性定义
+defineProps({
+  msg: String,
+});
+//3.定义事件
+const emit = defineEmits(['myclick'])
+const onclick = () =>{
+  emit('myclick')
+}
+
+const count = ref(0);
+</script>
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  alias: {
+    "@": path.resolve(__dirname, "src"),
+    comps: path.resolve(__dirname, "src/components"),
+    apis: path.resolve(__dirname, "src/apis"),
+    views: path.resolve(__dirname, "src/views"),
+    utils: path.resolve(__dirname, "src/utils"),
+    routes: path.resolve(__dirname, "src/routes"),
+    styles: path.resolve(__dirname, "src/styles "),
+  },
+  plugins: [vue()],
+});
